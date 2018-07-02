c98d467c82585d6d40d993f70208388b2d88cd60
Ethan <ethanyang4623@gmail.com>
2018-07-02 16:26:16 -0400

    Add boundary condition & verification logic

--- Golden_Model/RL_Model_top.m
+++ Golden_Model/RL_Model_top.m
@@ -36,7 +36,7 @@ GRAD_COEF = 0.291067;
 
 %% Load the data from input file
 input_file_path = strcat(common_path, input_position_file_name);
-fprintf('Start reading data from input file %s\n', input_file_path);
+fprintf('*** Start reading data from input file %s ***\n', input_file_path);
 position_data = load_particle_position(input_file_path);
 fprintf('Particle data loading finished!\n');
 % find the min, max on each dimension
@@ -51,13 +51,13 @@ max_z  = max(position_data(:,3));
 position_data(:,1) = position_data(:,1)-min_x;          % range: 0 ~ 112.533
 position_data(:,2) = position_data(:,2)-min_y;          % range: 0 ~ 113.382
 position_data(:,3) = position_data(:,3)-min_z;          % range: 0 ~ 81.489
-fprintf('Particles shifted to align on (0,0,0)\n');
+fprintf('All particles shifted to align on (0,0,0)\n');
 
 %% Mapping the particles to cell list
-fprintf('Start mapping paricles to cells!\n');
+fprintf('*** Start mapping paricles to cells! ***\n');
 % Bounding box of 12A, total of 9*9*7 cells, organized in a 4D array
 particle_in_cell_counter = zeros(CELL_COUNT_X,CELL_COUNT_Y,CELL_COUNT_Z);               % counters tracking the # of particles in each cell
-cell_particle = zeros(CELL_COUNT_X*CELL_COUNT_Y*CELL_COUNT_Z,CELL_PARTICLE_MAX,4);      % 3D array holding sorted cell particles(cell_id, particle_id, position_data & force value), cell_id = (cell_x-1)*9+(cell_y-1)*9+cell_z
+cell_particle = zeros(CELL_COUNT_X*CELL_COUNT_Y*CELL_COUNT_Z,CELL_PARTICLE_MAX,5);      % 3D array holding sorted cell particles(cell_id, particle_id, particle_info), cell_id = (cell_x-1)*9*7+(cell_y-1)*7+cell_z
 out_range_particle_counter = 0;
 for i = 1:TOTAL_PARTICLE
     % determine the cell each particle belongs to
@@ -69,17 +69,20 @@ for i = 1:TOTAL_PARTICLE
         % increment counter
         counter_temp = particle_in_cell_counter(cell_x, cell_y, cell_z) + 1;
         particle_in_cell_counter(cell_x, cell_y, cell_z) = counter_temp;
-        cell_id = (cell_x-1)*CELL_COUNT_X + (cell_y-1)*CELL_COUNT_Y + cell_z;
-        cell_particle(cell_id,counter_temp,1:3) = position_data(i,:);
+        cell_id = (cell_x-1)*CELL_COUNT_Y*CELL_COUNT_Z + (cell_y-1)*CELL_COUNT_Z + cell_z;
+        cell_particle(cell_id,counter_temp,1) = position_data(i,1);
+        cell_particle(cell_id,counter_temp,2) = position_data(i,2);
+        cell_particle(cell_id,counter_temp,3) = position_data(i,3);
     else
         out_range_particle_counter = out_range_particle_counter + 1;
     end
 end
 fprintf('Particles mapping to cells finished! Total of %d particles falling out of the range.\n', out_range_particle_counter);
 
-%% Filtering
+%% Filtering & Force Evaluation
 %%%%%%%%%%%%%%%%%%% In the current implementation, traverse through all the 27 neighbor cells for simple accumulation %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-fprintf('Filtering and evaluation starts!\n');
+fprintf('*** Filtering and evaluation starts! ***\n');
+force_write_back_counter = 0;       % Profiling variable to record how many neighbor particles has been calculated
 % Loopping into each cell
 for home_cell_x = 1:CELL_COUNT_X
     for home_cell_y = 1:CELL_COUNT_Y
@@ -109,13 +112,15 @@ for home_cell_x = 1:CELL_COUNT_X
             end
             
             % Collect the home cell information
-            home_cell_id = (home_cell_x-1)*CELL_COUNT_X + (home_cell_y-1)*CELL_COUNT_Y + home_cell_z;
+            home_cell_id = (home_cell_x-1)*CELL_COUNT_Y*CELL_COUNT_Z + (home_cell_y-1)*CELL_COUNT_Z + home_cell_z;
             home_particle_counter = particle_in_cell_counter(home_cell_x,home_cell_y,home_cell_z);
             
             %% Select each particle in home cell as reference particle, traver all the particles in home cells
             for ref_particle_ptr = 1:home_particle_counter
                 % Initialize the force value as 0 for each reference particle
                 Force_Acc = 0;
+                % Initialize the counter for recording how many neighboring particles within the cutoff radius
+                particles_within_cutoff = 0;
                 
                 % Get reference particle coordinates
                 ref_particle_pos_x = cell_particle(home_cell_id,ref_particle_ptr,1);
@@ -127,14 +132,34 @@ for home_cell_x = 1:CELL_COUNT_X
                     for neighbor_cell_y_ptr=1:3
                         for neighbor_cell_z_ptr=1:3
                             % Get the neighbor cell id
-                            neighbor_cell_id = (neighbor_cell_x_list(neighbor_cell_x_ptr)-1)*CELL_COUNT_X + (neighbor_cell_y_list(neighbor_cell_y_ptr)-1)*CELL_COUNT_Y + neighbor_cell_z_list(neighbor_cell_z_ptr);
-                            neighbor_particle_counter = particle_in_cell_counter(neighbor_cell_x_list(neighbor_cell_x_ptr),neighbor_cell_y_list(neighbor_cell_y_ptr),neighbor_cell_z_list(neighbor_cell_z_ptr));
+                            neighbor_cell_x = neighbor_cell_x_list(neighbor_cell_x_ptr);
+                            neighbor_cell_y = neighbor_cell_y_list(neighbor_cell_y_ptr);
+                            neighbor_cell_z = neighbor_cell_z_list(neighbor_cell_z_ptr);
+                            neighbor_cell_id = (neighbor_cell_x-1)*CELL_COUNT_Y*CELL_COUNT_Z + (neighbor_cell_y-1)*CELL_COUNT_Z + neighbor_cell_z;
+                            neighbor_particle_counter = particle_in_cell_counter(neighbor_cell_x,neighbor_cell_y,neighbor_cell_z);
                         
                             %% Traverse all the partner particles inside the current neighbor cell
                             for neighbor_particle_ptr=1:neighbor_particle_counter
                                 neighbor_particle_pos_x = cell_particle(neighbor_cell_id,neighbor_particle_ptr,1);
                                 neighbor_particle_pos_y = cell_particle(neighbor_cell_id,neighbor_particle_ptr,2);
                                 neighbor_particle_pos_z = cell_particle(neighbor_cell_id,neighbor_particle_ptr,3);
+                                
+                                % Apply boundary condition to the partner particles
+                                if home_cell_x == CELL_COUNT_X && neighbor_cell_x == 1
+                                    neighbor_particle_pos_x = neighbor_particle_pos_x + CELL_COUNT_X * CUTOFF_RADIUS;
+                                elseif home_cell_x == 1 && neighbor_cell_x == CELL_COUNT_X
+                                    neighbor_particle_pos_x = neighbor_particle_pos_x - CELL_COUNT_X * CUTOFF_RADIUS;
+                                end
+                                if home_cell_y == CELL_COUNT_Y && neighbor_cell_y == 1
+                                    neighbor_particle_pos_y = neighbor_particle_pos_y + CELL_COUNT_Y * CUTOFF_RADIUS;
+                                elseif home_cell_y == 1 && neighbor_cell_y == CELL_COUNT_Y
+                                    neighbor_particle_pos_y = neighbor_particle_pos_y - CELL_COUNT_Y * CUTOFF_RADIUS;
+                                end
+                                if home_cell_z == CELL_COUNT_Z && neighbor_cell_z == 1
+                                    neighbor_particle_pos_z = neighbor_particle_pos_z + CELL_COUNT_Z * CUTOFF_RADIUS;
+                                elseif home_cell_z == 1 && neighbor_cell_z == CELL_COUNT_Z
+                                    neighbor_particle_pos_z = neighbor_particle_pos_z - CELL_COUNT_Z * CUTOFF_RADIUS;
+                                end
                                 
                                 % Calculate the distance square btw the reference particle and parter particle
                                 dist_x_2 = (neighbor_particle_pos_x - ref_particle_pos_x)^2;
@@ -146,11 +171,13 @@ for home_cell_x = 1:CELL_COUNT_X
                                 A = 12 * EPSILON *(SIGMA^12);
                                 B = 6 * EPSILON *(SIGMA^6);
                                 if dist_2 <= CUTOFF_RADIUS_2 && dist_2 > 0
+                                    % increment the counter
+                                    particles_within_cutoff = particles_within_cutoff + 1;
+                                    % Force calculate (??????????????????? Is the formular here right ??????????????????)
                                     inv_dist_2 = 1 / dist_2;
                                     inv_dist_4 = inv_dist_2 ^ 2;
                                     inv_dist_8 = inv_dist_4 ^ 2;
                                     inv_dist_14 = inv_dist_4 * inv_dist_8 * inv_dist_2;
-                                    % Force calculate (??????????????????? Is the formular here right ??????????????????)
                                     Coulomb_Force = CC * erfc(EWALD_COEF*sqrt(dist_2)) * (1/sqrt(dist_2));
                                     LJ_Force = A * inv_dist_14 - B * inv_dist_8;
                                     
@@ -163,8 +190,88 @@ for home_cell_x = 1:CELL_COUNT_X
                 end
                 % Assign the total force to cell_particle
                 cell_particle(home_cell_id,ref_particle_ptr,4) = Force_Acc;
+                cell_particle(home_cell_id,ref_particle_ptr,5) = particles_within_cutoff;
+                force_write_back_counter = force_write_back_counter + 1;        % Profiling variable to record how many neighbor particles has been calculated
             end
             
         end
     end
 end
+fprintf('Force evaluation is finished!\n');
+
+%% Profiling & verification
+fprintf('*** Starting Verification! ***\n')
+% Verify the total number of evaluated particles
+valid_counter = 0;
+max_home_cell_id = 0;
+for home_cell_x = 1:CELL_COUNT_X
+    for home_cell_y = 1:CELL_COUNT_Y
+        for home_cell_z = 1:CELL_COUNT_Z
+            home_cell_id = (home_cell_x-1)*CELL_COUNT_Y*CELL_COUNT_Z + (home_cell_y-1)*CELL_COUNT_Z + home_cell_z;
+            if home_cell_id > max_home_cell_id
+                max_home_cell_id = home_cell_id;
+            end
+            particle_count = particle_in_cell_counter(home_cell_x,home_cell_y,home_cell_z);
+            for particle_ptr = 1:particle_count
+                if cell_particle(home_cell_id,particle_ptr,4) == 0
+                    fprintf('home cell %d, particle %d, force value missing!\n', home_cell_id,particle_ptr);
+                end
+                if cell_particle(home_cell_id,particle_ptr,4) ~= 0
+                    valid_counter = valid_counter + 1;
+                end
+            end
+        end
+    end
+end
+fprintf('Total particle number is %d, particles mapped into cell is %d, force write back counter is %d, parciles with valid force value is %d\n', TOTAL_PARTICLE, TOTAL_PARTICLE-out_range_particle_counter, force_write_back_counter, valid_counter);
+
+% Pick a reference paricle and evaluate (!!!!!!!!!!avoid the particles in the corner cells!!!!!!!!)
+home_cell_x = 3;
+home_cell_y = 4;
+home_cell_z = 3;
+particle_id = 1;
+if (home_cell_x == 1 || home_cell_x == CELL_COUNT_X || home_cell_y == 1 || home_cell_y == CELL_COUNT_Y || home_cell_z == 1 || home_cell_z == CELL_COUNT_Z)
+    fprintf('Error in verification: Please reselect the reference particle cell (avoid the corner cells)!!!!\n');
+    exit;
+end
+cell_id = (home_cell_x-1)*CELL_COUNT_Y*CELL_COUNT_Z + (home_cell_y-1)*CELL_COUNT_Z + home_cell_z;
+ref_particle_pos_x = cell_particle(cell_id, particle_id, 1);
+ref_particle_pos_y = cell_particle(cell_id, particle_id, 2);
+ref_particle_pos_z = cell_particle(cell_id, particle_id, 3);
+% Traverse across all the particles in the simulation space
+Force_Acc = 0;
+particles_within_cutoff = 0;
+for i = 1:TOTAL_PARTICLE
+    neighbor_particle_pos_x = position_data(i,1);
+    neighbor_particle_pos_y = position_data(i,2);
+    neighbor_particle_pos_z = position_data(i,3);
+    dist_x_2 = (neighbor_particle_pos_x - ref_particle_pos_x)^2;
+    dist_y_2 = (neighbor_particle_pos_y - ref_particle_pos_y)^2;
+    dist_z_2 = (neighbor_particle_pos_z - ref_particle_pos_z)^2;
+    dist_2 = dist_x_2 + dist_y_2 + dist_z_2;
+    % Filtering, and direct force evaluation
+    A = 12 * EPSILON *(SIGMA^12);
+    B = 6 * EPSILON *(SIGMA^6);
+    if dist_2 <= CUTOFF_RADIUS_2 && dist_2 > 0
+        particles_within_cutoff = particles_within_cutoff + 1;
+        % check if there are particles within cutoff radius but falling into far cells
+        neighbor_cell_x = ceil(neighbor_particle_pos_x / CUTOFF_RADIUS);
+        neighbor_cell_y = ceil(neighbor_particle_pos_y / CUTOFF_RADIUS);
+        neighbor_cell_z = ceil(neighbor_particle_pos_z / CUTOFF_RADIUS);
+        if neighbor_cell_x > home_cell_x + 1 || neighbor_cell_x < home_cell_x - 1 || neighbor_cell_y > home_cell_y + 1 || neighbor_cell_y < home_cell_y - 1 || neighbor_cell_z > home_cell_z + 1 || neighbor_cell_z < home_cell_z - 1
+            fprintf('Exceptions!! neighbor particle (%f,%f,%f) is not captured by reference particle (%f,%f,%f)....\n',neighbor_particle_pos_x,neighbor_particle_pos_y,neighbor_particle_pos_z,ref_particle_pos_x,ref_particle_pos_y,ref_particle_pos_z);
+        end
+        % Force evaluation
+        inv_dist_2 = 1 / dist_2;
+        inv_dist_4 = inv_dist_2 ^ 2;
+        inv_dist_8 = inv_dist_4 ^ 2;
+        inv_dist_14 = inv_dist_4 * inv_dist_8 * inv_dist_2;
+        % Force calculate (??????????????????? Is the formular here right ??????????????????)
+        Coulomb_Force = CC * erfc(EWALD_COEF*sqrt(dist_2)) * (1/sqrt(dist_2));
+        LJ_Force = A * inv_dist_14 - B * inv_dist_8;
+
+        % Accumulate the force
+        Force_Acc = Force_Acc + Coulomb_Force + LJ_Force;
+    end
+end
+fprintf('The simulated force is %f with %d particles in cutoff, the verification value is %f with %d particles in cutoff\n', cell_particle(cell_id,particle_id,4),cell_particle(cell_id,particle_id,5),Force_Acc,particles_within_cutoff);
